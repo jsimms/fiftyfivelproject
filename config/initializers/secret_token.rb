fc763e6681b746612ba60e8373004672ed0e8744
Jake Simms <jakedsimms@gmail.com>
2013-10-26 16:18:20 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fiftyfivelproject::Application.config.secret_key_base = 'e3c8017419f7682a006beab7ee17589fc74d6ee12f3ad036aa016d0eb48dafa36d38cc924775bc3fa7f07c302a7a7386e369cb41049734a4feb6a71d05395cb3'
