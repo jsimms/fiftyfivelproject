7b7c99efceb93687d2f09f459b5b6d922f28b291
Jake Simms <jakedsimms@gmail.com>
2013-10-29 15:45:51 -0400

    added bootstrap made header and footer partials

--- Gemfile
+++ Gemfile
@@ -5,6 +5,15 @@ ruby '2.0.0'
 gem 'rails', '4.0.0'
 gem 'pg', '0.15.1'
 
+gem 'sass-rails', '~> 4.0.0'
+gem 'bootstrap-sass', '~> 3.0.0.0.rc'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'bcrypt-ruby', '~> 3.0.0'
+
 group :development, :test do
   gem 'rspec-rails', '2.13.1'
   gem 'guard-rspec', '2.5.0'
@@ -18,14 +27,6 @@ group :test do
   gem 'capybara', '2.1.0'
   gem 'growl', '1.0.3'
 end
-
-gem 'sass-rails', '~> 4.0.0'
-gem 'uglifier', '>= 1.3.0'
-gem 'coffee-rails', '~> 4.0.0'
-gem 'jquery-rails'
-gem 'turbolinks'
-gem 'jbuilder', '~> 1.2'
-gem 'bcrypt-ruby', '~> 3.0.0'
 
 group :doc do
   gem 'sdoc', require: false
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,12 +6,12 @@ describe "StaticPages" do
 
   describe "Home page" do
     it "Should have the content 'People make the best guide books.'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('People make the best guide books.')
     end
 
     it "should have the right title" do
-  		visit '/static_pages/home'
+  		visit root_path
   		expect(page).to have_title("Make No Small Plans #{base_title}")
 		end
 
@@ -19,12 +19,12 @@ describe "StaticPages" do
 
   describe "About page" do
     it "Should have the content 'The internet is a room full of strangers. We're a bar full of friends.'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content("The internet is a room full of strangers. We're a bar full of friends.")
     end
 
     it "should have the right title" do
-  		visit '/static_pages/about'
+  		visit about_path
   		expect(page).to have_title("About the Project #{base_title}")
 		end
 
@@ -32,12 +32,12 @@ describe "StaticPages" do
 
   describe "Apply page" do
     it "Should have the content 'Wanna publish with the project?'" do
-      visit '/static_pages/apply'
+      visit apply_path
       expect(page).to have_content("Wanna publish with the project?")
     end
 
     it "should have the right title" do
-  		visit '/static_pages/apply'
+  		visit apply_path
   		expect(page).to have_title("Write for the Project #{base_title}")
 		end
 
